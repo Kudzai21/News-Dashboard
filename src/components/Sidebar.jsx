9f59826f2c7177d4dd264e5fa0deacb52e52a3c3
Oswell Kudzai Mapuranga <mapurangako@gmail.com>
2025-02-13 12:11:39 +0200

    ui sidebar changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Sidebar = ({ onFilter }) => {
   const categories = [
-    "Business",
-    "Entertainment",
-    "Health",
-    "Science",
-    "Sports",
-    "Technology",
+    "Business Insights",
+    "Entertainment Buzz",
+    "Health Care Focus",
+    "Science Insights",
+    "Sports Highlights",
+    "Technology Trends",
   ];
 
   return (
-    <div className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-64 shadow-md px-4 pt-28 overflow-y-auto z-10">
+    <div className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-64 shadow-md px-5 pt-28 overflow-y-auto z-10">
       <h2 className="text-2xl font-bold mb-4">Categories</h2>
       <ul>
         {categories.map((category) => (
